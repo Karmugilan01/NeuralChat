a0fd6b14ea3f0340ff340b5ab39fbf02cbf47458
Karmugilan N <160196275+Karmugilan01@users.noreply.github.com>
2026-05-17 11:15:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.set('trust proxy', 1);
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://neural-chat-ivory.vercel.app'
+  'https://neural-chat-ten.vercel.app/'
 ];
 
 app.use(cors({
